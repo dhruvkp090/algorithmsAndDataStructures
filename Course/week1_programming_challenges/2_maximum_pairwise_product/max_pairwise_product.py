23262ce225b31feba9bb28885eee17dd17a527c0
dhruvkp090 <221710302021@gitam.in>
2020-05-11 11:05:49 +0530

    Added a more efficient dunction for max pairwise product using sort

--- Course/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Course/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,5 +1,6 @@
 # python3
-
+from numpy.random import randint
+import numpy as np
 
 def max_pairwise_product(numbers):
     n = len(numbers)
@@ -12,7 +13,30 @@ def max_pairwise_product(numbers):
     return max_product
 
 
+def max_pairwise_product_faster(numbers):
+    numbers.sort(reverse=True)
+    return numbers[0]*numbers[1]
+
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product_faster(input_numbers))
+    """
+    Stress Test code below
+
+    """
+    # while True:
+    #     input_n = randint(0, 1000)
+    #     input_numbers = randint(0, 10000, input_n)
+    #     fast = max_pairwise_product_fast(input_numbers)
+    #     brute = max_pairwise_product(input_numbers)
+    #     if fast == brute:
+    #         print(str(fast) + " " + str(brute))
+    #         print(input_numbers)
+    #         print("OK")
+    #     else:
+    #         print(str(fast) + " " + str(brute))
+    #         print(input_numbers)
+    #         print("ERROR!!")
+    #         break
+
